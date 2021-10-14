0f2951a3e34f1cbcaed3cfe759f659cd1cd7dce3
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-10-14 18:47:16 -0300

    Commit inicial

---
+++ src/main/java/com/codanalise/CodAnaliseApplication.java
@@ -0,0 +1,13 @@
+package com.codanalise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodAnaliseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodAnaliseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codanalise/CodAnaliseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codanalise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodAnaliseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
