0105e052137041d7cddd309326ba822006927996
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-11-30 16:25:01 -0300

    Escolaridade com gets

--- src/main/java/com/codanalise/controller/EscolaridadeController.java
+++ src/main/java/com/codanalise/controller/EscolaridadeController.java
@@ -3,21 +3,39 @@ package com.codanalise.controller;
 import com.codanalise.model.Escolaridade;
 import com.codanalise.repository.EscolaridadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/esc")
 public class EscolaridadeController {
 
     @Autowired
-    EscolaridadeRepository esc;
+    EscolaridadeRepository escolaridadeRepository;
 
     @GetMapping
     public List<Escolaridade> listarEsc(){
-        return esc.findAll();
+        return escolaridadeRepository.findAll();
     }
+
+
+
+    @GetMapping(value ="/{id}")
+    public List<Escolaridade> escPorUsuario (@PathVariable Long id){
+
+        return escolaridadeRepository.findByUsuario_id(id);
+//        Optional<Escolaridade> escolaridade = escolaridadeRepository.findByUsuario_id(id);
+//        if (escolaridade.isPresent()){
+//            return ResponseEntity.ok(escolaridade.get());
+//        }
+//
+//        return ResponseEntity.notFound().build();
+    }
+
 }
--- src/main/java/com/codanalise/repository/EscolaridadeRepository.java
+++ src/main/java/com/codanalise/repository/EscolaridadeRepository.java
@@ -3,6 +3,17 @@ package com.codanalise.repository;
 
 import com.codanalise.model.Escolaridade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface EscolaridadeRepository extends JpaRepository<Escolaridade, Long> {
+
+
+//    @Query("select e from Escolaridade e where e.usuario_id = ?1")
+//    public Optional<Escolaridade> findByUsuario_id(Long usuario_id);
+
+    @Query("select e from Escolaridade e where e.usuario_id = ?1")
+    public List<Escolaridade> findByUsuario_id(Long usuario_id);
 }
